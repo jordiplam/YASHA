064cb423832afc2510d9a0f6e761de615a9eeee0
Jordi Pla <j.pla.mauri@gmail.com>
2020-05-09 21:11:43 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Yet Another Sci-Hub Add-on
---
+++ yasha.js
@@ -0,0 +1,33 @@
+/*
+ * Background script. Current webpage is openned prepending SCIHUB_URL.
+ * Requires "activeTag" permision to access the URL of the current tab.
+*/
+
+const SCIHUB_URL = "https://sci-hub.tw"
+
+/*
+ * If the Browser API is not defined, alias the Chrome API to the Browser API.
+ * This should make the extension compatible with Chromium-based browsers.
+*/
+if (typeof browser === "undefined") {
+	browser = chrome;
+}
+
+/*
+ * Log error message if an asynchronous operation fails.
+ */
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+/*
+ * Open new tab with SCIHUB_URL when the toolbar icon is clicked.
+ *
+ * Opening the article in the same tab could be achieved using:
+ * > browser.tabs.update(tab.id, {"url": NEW_URL})
+ */
+browser.browserAction.onClicked.addListener((tab) => {
+	browser.tabs.create({
+		"url": SCIHUB_URL + "/" + tab.url,
+	}).catch(onError);
+});
